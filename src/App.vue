7d867441d6e80c88af02b387a6a6555c833d72ae
Frynoodles <sanshuidesu@163.com>
2023-02-23 23:41:14 +0800

    又加了一个对状态的监听

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+import header_pannel from './components/Header.vue'
+(function(){
+  document.addEventListener('visibilitychange',function(){
+    if(!document.hidden){
+      // 处于当前页面
+      document.title="你所热爱的就是你的生活"
+    }else{
+      // 不在当前页面
+      document.title="(*^▽^*) ~~ 摸鱼"
+    }
+  })
+})();
+</script>
+
 <template>
   <header_pannel />
 </template>
 
-<script setup lang="ts">
-import header_pannel from './components/Header.vue'
-</script>
 
 <style scoped></style>

4fb4c03d740554188c525326fcae56375301f94f
Frynoodles <sanshuidesu@163.com>
2023-02-25 16:50:25 +0800

    试了一下vue的属性绑定

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max)
+}
+const test_color = ref('');
+const v_bind_test = () => {
+    test_color.value = `background-color:rgba(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)},${Math.random().toFixed(3).slice(0, -1)})`
+}
+
 </script>
 <template>
     <div class="main-content-box">
         <div class="main-content">
             <div class="column-box" id="left-box">
-                <div class="white-box"></div>
+                <div class="white-box" @click="v_bind_test" v-bind:style="test_color"></div>
                 <div class="white-box"></div>
             </div>
             <div class="column-box" id="right-box">
                 <div class="white-box"></div>
                 <div class="white-box"></div>
-                <img src="https://raw.githubusercontent.com/Frynoodles/frynoodles/main/%E9%A9%AC%E6%81%A9%E5%88%97%E6%96%AF%E6%AF%9B.jpg" alt="">
+                <img src="https://raw.githubusercontent.com/Frynoodles/frynoodles/main/%E9%A9%AC%E6%81%A9%E5%88%97%E6%96%AF%E6%AF%9B.jpg"
+                    alt="">
             </div>
         </div>
     </div>
